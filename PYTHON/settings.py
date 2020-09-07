da98cba8292f74eb8b08e8b314fbaff6cf679c27
sanix-darker <s4nixd@gmail.com>
2020-09-07 13:44:21 +0100

    Refactor configs

--- PYTHON/settings.py
+++ PYTHON/settings.py
@@ -1,11 +1,11 @@
 # First, install configparser using "pip install configparser"
-import configparser as ConfigParser
+import configparser
 
 # Configs parameters
-configParser = ConfigParser.RawConfigParser()   
+conf = configparser.RawConfigParser()   
 configFilePath = r'config.txt'
-configParser.read(configFilePath)
+conf.read(configFilePath)
 
 # Filling parameters
-CHAT_ID = configParser.get('oclients', 'CHAT_ID')
-HOST_URL = configParser.get('oclients', 'HOST_URL')
+CHAT_ID = conf.get('oclients', 'CHAT_ID')
+HOST_URL = conf.get('oclients', 'HOST_URL')
